e8f04088b1d200f3577ece4f89b0b60f4091b025
Francesco <francesco.catalanotti25@gmail.com>
2023-03-20 20:21:16 +0100

    Automatic patch and dkms installation
    Removed the option to install the patch manually. The patch is now installed automatically. The script will also install the dkms module automatically. Documentation has been updated to reflect these changes.

--- install.sh
+++ install.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
 if [ "$(id -u)" -ne 0 ]; then echo "Please run using sudo."; exit 1; fi
 
+# check if /usr/bin/patch or /usr/sbin/dkms is not installed
+if [[ ! -f /usr/bin/patch || ! -f /usr/sbin/dkms ]]; then
+    # check if is using apt, dnf, yum, pacman, zypper, apk or pkg 
+    if [ -f /usr/bin/apt ]; then
+        apt install patch
+        apt install dkms
+    elif [ -f /usr/bin/dnf ]; then
+        dnf install patch
+        dnf install dkms
+    elif [ -f /usr/bin/yum ]; then
+        yum install patch
+        yum install dkms
+    elif [ -f /usr/bin/pacman ]; then
+        pacman -S patch
+        pacman -S dkms
+    elif [ -f /usr/bin/zypper ]; then
+        zypper install patch
+        zypper install dkms
+    elif [ -f /usr/bin/apk ]; then
+        apk add patch
+        apk add dkms
+    elif [ -f /usr/bin/pkg ]; then
+        pkg install patch
+        pkg install dkms
+    else
+        echo "Please install patch and dkms."
+        exit 2
+    fi
+fi
+
 if [ -d /usr/src/asus-wmi-1.0 ] ; then
         while true; do
                 read -p 'There seems to be a version installed already. Remove (recommended)? [Y/n] ' answer
@@ -16,61 +46,29 @@ if [ -d /usr/src/asus-wmi-1.0 ] ; then
         done
 fi
 
-PATCH=false
+echo "Patching...";
 
-while getopts p flag
-do
-    case "${flag}" in
-        p) PATCH=true;;
-    esac
-done
+VERSION=`uname -r | grep -o '^[0-9]\+\.[0-9]\+'`
 
-if [ $PATCH = true ]; then
-    # check if /usr/bin/patch exists
-    if [ ! -f /usr/bin/patch ]; then
-        # check if is using apt, dnf, yum, pacman, zypper, apk or pkg 
-        if [ -f /usr/bin/apt ]; then
-            apt install patch
-        elif [ -f /usr/bin/dnf ]; then
-            dnf install patch
-        elif [ -f /usr/bin/yum ]; then
-            yum install patch
-        elif [ -f /usr/bin/pacman ]; then
-            pacman -S patch
-        elif [ -f /usr/bin/zypper ]; then
-            zypper install patch
-        elif [ -f /usr/bin/apk ]; then
-            apk add patch
-        elif [ -f /usr/bin/pkg ]; then
-            pkg install patch
-        else
-            echo "Please install patch."
-            exit 2
-        fi
-    fi
-    echo "Patching...";
-
-    VERSION=`uname -r | grep -o '^[0-9]\+\.[0-9]\+'`
-
-    if { echo $VERSION ; echo "5.7" ; } | sort -V -c 2>/dev/null
-    then
-    PATCHFILE="patch"
-    elif { echo $VERSION ; echo "5.99" ; } | sort -V -c 2>/dev/null
-    then
-    PATCHFILE="patch5.8"
-    else
-    PATCHFILE="patch6.0"
-    fi
-
-    echo "Using: $PATCHFILE"
-
-    wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O 'asus-wmi.c'
-    wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.h?h=linux-$VERSION.y" -O 'asus-wmi.h'
-    wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-nb-wmi.c?h=linux-$VERSION.y" -O 'asus-nb-wmi.c'
-    patch -p1 < $PATCHFILE
-    rm *.orig
-    echo "Patch complete.";
+if { echo $VERSION ; echo "5.7" ; } | sort -V -c 2>/dev/null
+then
+PATCHFILE="patch"
+elif { echo $VERSION ; echo "5.99" ; } | sort -V -c 2>/dev/null
+then
+PATCHFILE="patch5.8"
+else
+PATCHFILE="patch6.0"
 fi
+
+echo "Using: $PATCHFILE"
+
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.c?h=linux-$VERSION.y" -O 'asus-wmi.c'
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-wmi.h?h=linux-$VERSION.y" -O 'asus-wmi.h'
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/platform/x86/asus-nb-wmi.c?h=linux-$VERSION.y" -O 'asus-nb-wmi.c'
+patch -p1 < $PATCHFILE
+rm *.orig
+echo "Patch complete.";
+
 
 #downloading zip and unpacking it
 mkdir /usr/src/asus-wmi-1.0
